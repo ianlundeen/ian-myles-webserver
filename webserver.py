e0ed9d625751a0d187dffce5523b9c6700a6a2a4
ianlundeen <52870002+ianlundeen@users.noreply.github.com>
2020-09-09 13:52:41 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -8,7 +8,7 @@ class Serv(BaseHTTPRequestHandler):
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
         except:
-            file_to_open = 'File not found or does not exist'
+            file_to_open = 'Wow it worked'
             self.send_response(404)
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
