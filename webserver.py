60cca1e9d84baba93154a96c665266fe32f578dc
MP <70173116+mylesphung@users.noreply.github.com>
2020-09-09 14:07:36 -0400

    The URL is http://127.0.0.1:5000/
    Use this instead of the previous web server file I uploaded

--- webserver.py
+++ webserver.py
@@ -1,17 +1,11 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+## Url is http://127.0.0.1:5000/
+from flask import Flask
 
-class Serv(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.path = '/index.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = 'Wow it worked'
-            self.send_response(404)
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+app = Flask(__name__)
 
-httpd = HTTPServer(('localhost', 8080), Serv)
-httpd.serve_forever()
+@app.route('/')
+def index():
+    return "Welcome to Myles' Flask Page!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host = "0.0.0.0")
