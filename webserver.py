1f41c5533f05eac1ed6df58e03012f890a9a04b2
MP <70173116+mylesphung@users.noreply.github.com>
2020-09-09 16:51:11 -0400

    Files for web server
    The py server is the script itself, the 2 html files are the different pages of the website and the css file is the coloring/style of the website

--- webserver.py
+++ webserver.py
@@ -1,11 +1,18 @@
-## Url is http://127.0.0.1:5000/
-from flask import Flask
+# Url is http://127.0.0.1:5000/
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "Welcome to Myles' Flask Page!"
+    return render_template('index.html')
+
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
